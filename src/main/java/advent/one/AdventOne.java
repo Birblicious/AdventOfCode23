c083f617d92f31328e5d4cb4d96d21ee7424eab4
Arda Burak Ekmekçi <ardaburak1992@hotmail.com>
2023-12-07 02:01:16 +0100

    init

---
+++ src/main/java/advent/one/AdventOne.java
@@ -0,0 +1,11 @@
+package advent.one;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventOne {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/advent/one/input/CalibrationDocument.java
@@ -0,0 +1,10 @@
+package advent.one.input;
+
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class CalibrationDocument {
+
+}
